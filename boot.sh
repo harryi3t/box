b10834c2767b21ab03dd163ae0a976e8e660cbba
Harendra Singh <66hari@gmail.com>
2016-04-30 12:52:52 +0530

    uses only one env variable

--- boot.sh
+++ boot.sh
@@ -2,27 +2,31 @@
 
 checkEnvironmentVariables() {
   echo 'Checking for required environment variables....'
-  if [[ -z $DB_USERNAME ]]; then
-    echo 'DB_USERNAME not found. Exiting.'
-    exit 1;
-  elif [[ -z $DB_PASSWORD ]]; then
-    echo 'DB_PASSWORD not found. Exiting.'
-    exit 1;
-  elif [[ -z $DB_HOST ]]; then
-    echo 'DB_HOST not found. Exiting.'
-    exit 1;
-  elif [[ -z $DB_PORT ]]; then
-    echo 'DB_PORT not found. Exiting.'
-    exit 1;
-  elif [[ -z $DB_NAME ]]; then
-    echo 'DB_NAME not found. Exiting.'
+  if [[ -z $DB_URL ]]; then
+    echo 'DB_URL not found. Exiting.'
     exit 1;
   fi
+  # if [[ -z $DB_USERNAME ]]; then
+  #   echo 'DB_USERNAME not found. Exiting.'
+  #   exit 1;
+  # elif [[ -z $DB_PASSWORD ]]; then
+  #   echo 'DB_PASSWORD not found. Exiting.'
+  #   exit 1;
+  # elif [[ -z $DB_HOST ]]; then
+  #   echo 'DB_HOST not found. Exiting.'
+  #   exit 1;
+  # elif [[ -z $DB_PORT ]]; then
+  #   echo 'DB_PORT not found. Exiting.'
+  #   exit 1;
+  # elif [[ -z $DB_NAME ]]; then
+  #   echo 'DB_NAME not found. Exiting.'
+  #   exit 1;
+  # fi
   echo 'All variables are present.'
 }
 exportVariables() {
   export readonly RANDOM_NUMBER=$(date +%s-%N)
-  export readonly DB_URL="mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
+  # export readonly DB_URL="mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 }
 startCron() {
   cd /home/harry/box
